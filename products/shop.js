4ef86f8a3b1703f620e745a5f987a237819395cc
joyboukaram <joy_boukaram@hotmail.com>
2018-11-25 12:23:23 +0200

    final

--- products/shop.js
+++ products/shop.js
@@ -120,3 +120,14 @@ const ad = document.querySelector("#ad");
 ad.addEventListener("click", event => {
   window.open("ad.html", "_self");
 });
+
+const cart = document.querySelector("#cart");
+cart.addEventListener("click", event => {
+  window.open("cart.html", "_self");
+})
+
+const searchpic = document.querySelector("#searchpic");
+const search = document.querySelector("#search");
+searchpic.addEventListener("click", event => {
+  search.style.border = "4px solid rgb(109, 44, 44)";
+})
